cf860b7e49ca4ed2bc1111566d388714a599a7ad
Sean Reed <sean@cheapfit.me>
2014-03-21 14:26:06 -0400

    Working on class Invariants

---
+++ pyconditions/invariant.py
@@ -0,0 +1,48 @@
+from functools import wraps
+
+def wrap_method( func ):
+  @wraps( func )
+  def wrapper( *args, **kwargs ):
+    ret = func( *args, **kwargs )
+
+    for invariant in args[0].__invariant__:
+      if( not invariant[1]( args[0] ) ):
+        raise PyCondition( "Invariant %s did not hold for %s" % ( invariant[0], args[0] ) )
+
+    return ret
+
+  return wrapper
+
+class InvariantMeta( type, object ):
+  def __new__(cls, name, bases, attrs):
+    for a in attrs:
+      if( a != "__metaclass__" and hasattr( attrs[a], "__call__" ) ):
+        attrs[a] = wrap_method( attrs[a] )
+
+    return super( InvariantMeta, cls).__new__( cls, name, bases, attrs)
+
+class Invariant( object ):
+  def __init__( self, name ):
+    self.name = name
+    self.condition = lambda a: True
+
+  def __call__( self, klass ):
+    dct = dict( klass.__dict__ )
+
+    if( not hasattr( klass, "__invariant__" ) ):
+      dct["__invariant__"] = []
+    dct["__invariant__"].append( ( self.name, self.condition, ) )
+    dct["__metaclass__"] = InvariantMeta
+
+    t = InvariantMeta( klass.__name__, klass.__bases__, dct )
+
+    return t
+
+class NoopInvariant( Invariant ):
+  def __init__( self, name ):
+    super( NoopInvariant, self ).__init__( name )
+
+    self.condition = lambda a: True
+
+  def assertInvariant( self, s ):
+    return self.condition( s )
---
+++ pyconditions/test/test_Invariant.py
@@ -0,0 +1,24 @@
+import unittest
+
+from ..invariant import Invariant, NoopInvariant, InvariantMeta
+
+class TestInvariant( unittest.TestCase ):
+
+  def test_Invariant_properly_sets_metaclass_and_invariant_list( self ):
+    @NoopInvariant( "test" )
+    @NoopInvariant( "test2" )
+    class Test( object ):
+      pass
+
+    self.assertEquals( InvariantMeta, Test.__metaclass__ )
+    self.assertEquals( [ "test2", "test" ], [ i[0] for i in Test.__invariant__ ] )
+
+  def test_Invariant_properly_wraps_each_method( self ):
+    @NoopInvariant( "test" )
+    class Test( object ):
+      def test( self ):
+        pass
+
+    t = Test()
+
+    t.test()
