9b0bfc0061af2b85af318d494ccb714c436e1575
Sean Reed <sean@cheapfit.me>
2014-03-21 17:26:57 -0400

    Upped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyConditions',
-    version='0.2.0',
+    version='0.2.1',
     author='Sean Reed',
     author_email='streed@mail.roanoke.edu',
     packages=['pyconditions', 'pyconditions.test'],
