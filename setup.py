3365f248db28f99a25c4ed9b2a85d0636ab7eb05
Sean Reed <sean@cheapfit.me>
2014-03-25 10:56:49 -0400

    Added classifiers to the package.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyConditions',
-    version='0.3.3',
+    version='0.3.5',
     author='Sean Reed',
     author_email='streed@mail.roanoke.edu',
     packages=['pyconditions', 'pyconditions.test'],
@@ -10,4 +10,15 @@ setup(
     license='LICENSE.txt',
     description='Commenting sucks so let your code do it for you with Guava like preconditions that also actually do something.',
     long_description=open('README.rst').read(),
+    classifiers=[
+      "Development Status :: 4 - Beta",
+      "License :: OSI Approved :: Apache Software License",
+      "Programming Language :: Python :: 2.6",
+      "Programming Language :: Python :: 2.7",
+      "Programming Language :: Python :: 3.2",
+      "Programming Language :: Python :: 3.3",
+      "Programming Language :: Python :: Implementation :: CPython",
+      "Programming Language :: Python :: Implementation :: PyPy",
+      "Topic :: Software Development :: Quality Assurance"
+    ],
 )
